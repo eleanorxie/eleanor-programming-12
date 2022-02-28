fe887a61ed0356c48e41ee49f1c8d55324e1d2c9
eleanorxie <99051921+eleanorxie@users.noreply.github.com>
2022-02-28 15:04:13 -0800

    2.28

--- Module 1/Test Directory/com/eleanorxie/module1assignment/module1point4/TriangleTest.java
+++ Module 1/Test Directory/com/eleanorxie/module1assignment/module1point4/TriangleTest.java
@@ -19,7 +19,7 @@ class TriangleTest {
         Triangle triangle1 = new Triangle(4.0, 6.0,5.0, Colour.RED);
         double semiPerimeter1 = (4.0 + 6.0 + 5.0) / 2;
         assertEquals(Math.sqrt(semiPerimeter1 * (semiPerimeter1 - 4.0) * (semiPerimeter1 - 6.0) * (semiPerimeter1 - 5.0)), triangle1.getArea());
-        
+
         //width = 2.0, height = 4.0
         Triangle triangle2 = new Triangle(2.0, 4.0, Colour.RED);
         double area2 = 2.0 * 4.0 / 2;
