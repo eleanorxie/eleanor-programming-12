c704b5b3eb0be46c93ffef91c2dd92b15bfce722
eleanorxie <2205925@learn.vsb.bc.ca>
2022-03-09 14:28:37 -0800

    3.9 sets

--- Sets Assignment/src/Main.java
+++ Sets Assignment/src/Main.java
@@ -1,41 +1,28 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
-import java.util.Locale;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
+
         Scanner scanner = new Scanner(new File("illiad.txt"));
-        while(scanner.hasNext()){
-            System.out.println(scanner.nextLine());
+        HashSet<String> wordSet = new HashSet<>();
+        int num = 0;
+        while(scanner.hasNextLine()){
+            String line = scanner.nextLine();
+            String[] words = line.split("\\W+");
+            for (int i = 0; i < words.length; i++) {
+                if(wordSet.add(words[i].toLowerCase())){
+                    num++;
+                }
+            }
         }
 
-        /*LinkedHashSet<Word> words = new LinkedHashSet<>();
-
-        ArrayList arr = new ArrayList<>();
-        String file = String.valueOf(new File("illiad.txt"));
-        String str = file;
-
-        while(str.length() > 0){
-            int j = str.indexOf(" ");
-            str = str.substring(j);
-            arr.add(str.substring((i,j)));
-
-
-        }*/
-
-        String str = String.valueOf(new File("illiad.txt"));
-        String[] words = str.replaceAll("\\p{Punct}", "").split(" ");
-        LinkedHashSet<String> wordSet = new LinkedHashSet<>();
-        for (int i = 0; i < words.length; i++) {
-            wordSet.add(words[i].toLowerCase(Locale.ROOT));
-        }
         for (String w:
              wordSet) {
             System.out.println(w);
         }
 
+        System.out.println(num);
     }
 }
