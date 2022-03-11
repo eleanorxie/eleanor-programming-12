6f25f4d86e308a2d7dbd701bd86d47e8dfbbfefc
eleanorxie <2205925@learn.vsb.bc.ca>
2022-03-11 14:43:34 -0800

    3.11

--- Map Assignment/src/CaesarCipher.java
+++ Map Assignment/src/CaesarCipher.java
@@ -1,23 +1,69 @@
-import java.lang.reflect.Array;
 import java.util.LinkedHashMap;
 
 public class CaesarCipher {
     private String word;
     private int numShift;
-
+    LinkedHashMap<String, String> mapEncrypt = new LinkedHashMap<>();
+    LinkedHashMap<String, String> mapDecrypt = new LinkedHashMap<>();
+    String[] str = "abcdefghijklmnopqrstuvwxyz".split("");
     public CaesarCipher(String word, int numShift) {
         this.word = word;
         this.numShift = numShift;
+        if(numShift > 0){
+            encrypt();
+            encryptWord();
+        }
+        else{
+            decrypt();
+            decryptWord();
+        }
+
+
     }
 
     private void encrypt(){
-        LinkedHashMap<String, String> mapEncrypt = new LinkedHashMap<>();
-        String[] str = "abcdefghijklmnopqrstuvwxyz".split("");
         for (int i = 0; i < 26; i++) {
-            mapEncrypt.put(str[i], (str[i + numShift]));
-            if(i == 25){
-                mapEncrypt.put(str[i], (str[numShift]));
+            int totalShift = i + numShift;
+
+            if(totalShift > 25){
+                totalShift-=25;
             }
+            mapEncrypt.put(str[i], (str[totalShift]));
+        }
+    }
+
+    private void decrypt(){
+        for (int i = 0; i < 26; i++) {
+            int totalShift = i + numShift;
+
+            if(totalShift < 0){
+                totalShift = 26 + totalShift;
+            }
+            mapDecrypt.put(str[i], (str[totalShift]));
+        }
+    }
+
+    private String encryptWord(){
+        String finalWord = "";
+        for(int i = 0; i < word.length(); i++){
+            finalWord = finalWord + mapEncrypt.get(word.substring(i,i+1));
+        }
+        return finalWord;
+    }
+    private String decryptWord(){
+        String finalWord = "";
+        for(int i = 0; i < word.length(); i++){
+            finalWord = finalWord + mapDecrypt.get(word.substring(i,i+1));
+        }
+        return finalWord;
+    }
+
+    public String toString() {
+        if(numShift > 0){
+            return encryptWord();
+        }
+        else{
+            return decryptWord();
         }
     }
 }
--- Map Assignment/src/Main.java
+++ Map Assignment/src/Main.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-
+        CaesarCipher caesarCipher = new CaesarCipher("abc", 14);
+        System.out.println(caesarCipher);
     }
 }
